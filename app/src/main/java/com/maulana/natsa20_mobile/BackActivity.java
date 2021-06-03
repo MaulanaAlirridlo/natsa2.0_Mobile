0538b853634176861a9a8438bce9b2c673556fc8
MaulanaAlirridlo <59948773+MaulanaAlirridlo@users.noreply.github.com>
2021-06-03 23:15:05 +0700

    Add about page

--- app/src/main/java/com/maulana/natsa20_mobile/BackActivity.java
+++ app/src/main/java/com/maulana/natsa20_mobile/BackActivity.java
@@ -5,34 +5,42 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Intent;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class BackActivity extends AppCompatActivity {
 
-    private String page;
+    String page;
+    TextView title;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_back);
 
+        title = findViewById(R.id.title);
+
         Bundle extras = getIntent().getExtras();
         page = extras.getString("page");
         switch (page) {
             case "about" :
-
+                loadFragment(new AboutFragment());
+                title.setText("About");
             break;
             case "FAQ" :
                 loadFragment(new FaqFragment());
+                title.setText("FAQ");
             break;
             case "contact" :
                 loadFragment(new ContactFragment());
+                title.setText("Our Contact");
             break;
             case "email" :
                 loadFragment(new EmailFragment());
+                title.setText("Send Email");
             break;
         }
     }
