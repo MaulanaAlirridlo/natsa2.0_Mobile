a44d0f49d4a1e4e30f1f8790ae8d4d35e44b17f4
MaulanaAlirridlo <59948773+MaulanaAlirridlo@users.noreply.github.com>
2021-06-10 12:13:11 +0700

    Add login, register page

---
+++ app/src/main/java/com/maulana/natsa20_mobile/activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.maulana.natsa20_mobile.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.maulana.natsa20_mobile.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
