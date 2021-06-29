71c3acc002f4c8568e4b1516a82610371d994cd3
MaulanaAlirridlo <59948773+MaulanaAlirridlo@users.noreply.github.com>
2021-06-29 13:51:59 +0700

    Add : Halaman tambah sawah

--- app/src/main/java/com/natsa/natsa20_mobile/fragment/Account/JualSewakanFragment.java
+++ app/src/main/java/com/natsa/natsa20_mobile/fragment/Account/JualSewakanFragment.java
@@ -3,17 +3,38 @@ package com.natsa.natsa20_mobile.fragment.Account;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.natsa.natsa20_mobile.R;
+import com.natsa.natsa20_mobile.fragment.AddProductFragment;
 
 public class JualSewakanFragment extends Fragment {
+
+    FloatingActionButton showAddProductForm;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_jual_sewakan, container, false);
+        View view = inflater.inflate(R.layout.fragment_jual_sewakan, container, false);
+        showAddProductForm = view.findViewById(R.id.show_add_product_form);
+
+        showAddProductForm.setOnClickListener(v -> {
+            loadFragment(new AddProductFragment());
+        });
+        return view;
+    }
+
+    //fragment loader
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fm = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.accountFrameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
---
+++ app/src/main/java/com/natsa/natsa20_mobile/fragment/AddProductFragment.java
@@ -0,0 +1,22 @@
+package com.natsa.natsa20_mobile.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.natsa.natsa20_mobile.R;
+
+public class AddProductFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_add_product, container, false);
+
+        return view;
+    }
+}
